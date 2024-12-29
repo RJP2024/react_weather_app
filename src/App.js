8b4f91517abf82dcd2636b2b0bf1c710ad2bb1a0
RJP2024 <ruth.jeanphilippe@gmail.com>
2024-12-28 21:49:05 -0500

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.ruth.io/" target="_blank">
+          Ruth Jean-Philippe
+        </a>{" "}
+        and is {" "}
+        <a 
+        href="https://github.com/RJP2024/react_weather_app"
+        target="_blank"
+        >
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
