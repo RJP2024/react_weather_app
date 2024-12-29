ada86b9cae3800a93492c056ed90e68fd7fcc915
RJP2024 <ruth.jeanphilippe@gmail.com>
2024-12-29 16:00:34 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.ruth.io/" target="_blank">
+      <div className="container">
+       <h1>Weather App</h1>
+       <footer>
+         This project was coded by{" "}
+         <a href="https://www.ruth.io/">
           Ruth Jean-Philippe
         </a>{" "}
         and is {" "}
         <a 
-        href="https://github.com/RJP2024/react_weather_app"
-        target="_blank"
-        >
+        href="https://github.com/RJP2024/react_weather_app">
           open-sourced on github
         </a>
       </footer>
     </div>
+  </div>
   );
 }
