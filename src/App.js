bfa9737010716517816c680c418676ec0b18994b
RJP2024 <ruth.jeanphilippe@gmail.com>
2024-12-29 17:42:06 -0500

    Added search button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <h1>Weather App</h1>
+       <Weather />
        <footer>
          This project was coded by{" "}
          <a href="https://www.ruth.io/">
