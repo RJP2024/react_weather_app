e4a40a8003a15377e3f4c4826177b5d3a563ca96
RJP2024 <ruth.jeanphilippe@gmail.com>
2024-12-29 21:49:44 -0500

    Everything added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <Weather />
+       <Weather defaultCity="New York" />
+
        <footer>
          This project was coded by{" "}
          <a href="https://www.ruth.io/">
@@ -16,6 +17,11 @@ export default function App() {
         <a 
         href="https://github.com/RJP2024/react_weather_app">
           open-sourced on github
+        </a>{" "}
+          and{" "}
+          <a
+            href="https://lucent-alpaca-b48221.netlify.app">
+            hosted on Netlify
         </a>
       </footer>
     </div>
