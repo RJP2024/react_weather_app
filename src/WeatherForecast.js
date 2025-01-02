d4f1c7972e0075632dd139494c60cb8fe94e8bcc
RJP2024 <ruth.jeanphilippe@gmail.com>
2025-01-01 20:14:27 -0500

    Updated apiurl & key

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
